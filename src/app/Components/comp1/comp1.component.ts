fcbb046915c2eb5ecd5d4c3bafc775da67d54abd
gsudan <ve00ym154@globalymc.com>
2025-03-13 17:08:14 +0530

    CI/CD Trial

--- src/app/Components/comp1/comp1.component.ts
+++ src/app/Components/comp1/comp1.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./comp1.component.scss']
 })
 export class Comp1Component {
-  msg: string = "Welcome";
+  msg: string = "Welcome CI/CD Trial";
   myModel="";
  public user: string = ""
  showLoginbtn: boolean = false;
